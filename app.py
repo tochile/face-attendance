31f5e7c307a158b62749c687219a6f11052e5120
Ezekiel Sochima <56741405+tochile@users.noreply.github.com>
2021-05-29 13:57:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ import yaml
 import numpy as np
 #import face_recognition 
 #import os
-
+#import cv2
 
 
 app = Flask(__name__) 
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
 
 
 
@@ -18,7 +24,18 @@ def index():
     return render_template("index.html")
 
 
-
+@app.route('/login',methods=['POST'])
+def login():
+    
+    username = request.form['username']
+    password = request.form['password']
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * from Users where binary username=%s and binary password=%s",[username,password])
+    if(result>0):
+        return render_template("login.html", username=username)
+    else:
+        error = 'failed'
+        return render_template("index.html", error=error)
 
 
            
