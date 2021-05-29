f40961f3d1e8471c700e4b5927e3a25de09a60cd
Ezekiel Sochima <56741405+tochile@users.noreply.github.com>
2021-05-29 13:41:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,11 @@ from flask_mysqldb import MySQL
 import yaml
 import numpy as np
 #import face_recognition 
-import os
+#import os
 
 
 
 app = Flask(__name__) 
-db = yaml.load(open('db.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
-mysql = MySQL(app)
 
 
 
